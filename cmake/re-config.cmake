8b24bc8f4ff5d399bd62eb231df15e78888f1dc3
Christian Spielberger <c.spielberger@commend.com>
2023-07-10 11:15:59 +0200

    cmake: MBEDTLS - use uppercase letters for find_package variables

--- cmake/re-config.cmake
+++ cmake/re-config.cmake
@@ -15,7 +15,7 @@ find_package(OpenSSL "1.1.1")
 endif()
 
 option(USE_OPENSSL "Enable OpenSSL" ${OPENSSL_FOUND})
-option(USE_MBEDTLS "Enable MbedTLS" ${MbedTLS_FOUND})
+option(USE_MBEDTLS "Enable MbedTLS" ${MBEDTLS_FOUND})
 option(USE_UNIXSOCK "Enable Unix Domain Sockets" ON)
 option(USE_TRACE "Enable Tracing helpers" OFF)
 
